4567090e5800487a03146b717821a6d8e81862d5
devparekh24 <devparekh2424@gmail.com>
2023-11-20 17:09:44 +0530

    productModel

---
+++ model/productModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Product must has a title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Product must has a description']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product must has a price']
+    },
+    category: {
+        type: String,
+        required: [true, 'Product must has a category']
+    },
+    thumbnail: {
+        type: String,
+        required: [true, 'Product must has a thumbnail']
+    },
+    discountPercentage: {
+        type: Number
+    },
+    rating: {
+        type: Number
+    },
+    stock: {
+        type: Number
+    },
+    brand: {
+        type: String
+    },
+    images: {
+        type: [String]
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product;
