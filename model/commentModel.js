462384639f5f48a2fc5fd99e1997b3cb5f9fab61
devparekh24 <devparekh2424@gmail.com>
2023-11-22 12:13:33 +0530

    comment Model

---
+++ model/commentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: [true, 'Comment cann\'t be empty!']
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: [true, 'Comment must belong to a product']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Comment must belong to a user']
+    }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment
